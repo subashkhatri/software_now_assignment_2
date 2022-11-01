e02b41e55fac2b6f7f6ba4472a006af4a8b4bf72
subasskhatrii <subash@teksewa.com>
2022-11-01 17:33:45 +0930

    Import random library create player create invaders

--- invade_space.py
+++ invade_space.py
@@ -2,6 +2,7 @@
 # Game developed by Subash khatri and Ram Kumar Shrestha
 # 01/11/2022
 
+import random
 import turtle
 
 # Setup the Screen
@@ -42,3 +43,35 @@ score_string = "Score: %s" %score
 draw_score.write(score_string, False, align="left", font=("Arial", 14, "normal"))
 draw_score.hideturtle()
 
+
+# Create the player turtle
+protector = turtle.Turtle()
+#protector.color("blue")
+protector.shape("protector.gif")
+protector.penup()
+protector.speed(0)
+protector.setposition(0,-250)
+protector.setheading(90)
+
+protector_speed = 15
+
+# Choose a number of invaders
+number_of_invaders = 10
+# Create an empty list of invaders
+invaders = []
+
+# Add invaders to the list
+for i in range(number_of_invaders):
+    # create the enemy
+    invaders.append(turtle.Turtle())
+
+for enemy in invaders:
+    #enemy.color("Red")
+    enemy.shape("invader.gif")
+    enemy.penup()
+    enemy.speed(0)
+    x = random.randint(-200, 200)
+    y =  random.randint(100, 250)
+    enemy.setposition(x, y)
+
+enemyspeed = 5
