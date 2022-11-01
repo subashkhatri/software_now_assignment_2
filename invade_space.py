42c4c5b5c7d2a2c32e4828eee11bb93f90c7ae3e
subasskhatrii <subash@teksewa.com>
2022-11-01 17:21:38 +0930

    Draw border - Add fix width and height for the screen where the actual game play is happening

--- invade_space.py
+++ invade_space.py
@@ -6,7 +6,7 @@ import turtle
 
 # Setup the Screen
 window = turtle.Screen()
-window.bgcolor("white")
+window.bgcolor("black")
 window.title("Invaders by Subash and Ram")
 window.bgpic("background.gif")
 
@@ -17,5 +17,16 @@ turtle.register_shape("invader.gif")
 turtle.register_shape("player.gif")
 
 # Draw Border
+border = turtle.Turtle()
+border.speed(0)
+border.color("white")
+border.penup()
+border.setposition(-300,-300)
+border.pendown()
+border.pensize(3)
+for side in range(4):
+    border.fd(600)
+    border.lt(90)
+border.hideturtle()
 
 # Initial Score be 0
